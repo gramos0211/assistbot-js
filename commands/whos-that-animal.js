b393fb63692570aecee27e102200aafc64af2a40
gramos0211 <45494272+gramos0211@users.noreply.github.com>
2022-03-25 17:05:41 -0400

    add nasa functionality

---
+++ commands/whos-that-animal.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageAttachment } = require('discord.js')
+
+const fs = require('fs');
+const animalPics = fs.readdirSync('./assets/animal-pics/')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('whos-that-animal')
+		.setDescription('Replies with random animal picture.'),
+	async execute(interaction) {
+		let chosenFile = animalPics[Math.floor(Math.random() * animalPics.length)];
+        const attachment = new MessageAttachment(`./assets/animal-pics/${chosenFile}`);
+		await interaction.reply({ files: [attachment]});
+	},
+};
