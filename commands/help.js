92d9798208be0d382d4a899fc510447ef51dee98
gramos0211 <45494272+gramos0211@users.noreply.github.com>
2022-03-26 23:00:56 -0400

    add image to help menu

--- commands/help.js
+++ commands/help.js
@@ -12,6 +12,7 @@ module.exports = {
         let embeddedHelpMsg = new MessageEmbed()
             .setColor("YELLOW")
             .setTitle("Help Menu")
+            .setThumbnail("https://media2.giphy.com/media/l0Ex3lGUCkofpN5UQ/giphy.gif?cid=790b7611cffbbe0a7ce0534ba94deff2aaea442d28f3ad0e&rid=giphy.gif&ct=g")
 
         for (const file of commandFiles) {
             let command = require(`../commands/${file}`);
