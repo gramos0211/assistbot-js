e06099f2ef18f9c50430c8bc89c4ef082a1aefcc
gramos0211 <45494272+gramos0211@users.noreply.github.com>
2022-03-26 23:05:02 -0400

    New Feature: help command (#3)
    Added help command. Sends embedded message with commands. Command fields are dynamically added with new command files in ./commands/

---
+++ commands/help.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const fs = require('node:fs');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Get available commands and descriptions'),
+	async execute(interaction) {
+        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+        let embeddedHelpMsg = new MessageEmbed()
+            .setColor("YELLOW")
+            .setTitle("Help Menu")
+            .setThumbnail("https://media2.giphy.com/media/l0Ex3lGUCkofpN5UQ/giphy.gif?cid=790b7611cffbbe0a7ce0534ba94deff2aaea442d28f3ad0e&rid=giphy.gif&ct=g")
+
+        for (const file of commandFiles) {
+            let command = require(`../commands/${file}`);
+
+            embeddedHelpMsg.addField(
+                `${command.data.name}`,
+                `${command.data.description}`
+            )
+        }
+
+        await interaction.reply({ embeds: [embeddedHelpMsg] })
+	},
+};
+
